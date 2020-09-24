9b6317e5d3241167c2dc87a0f087a7e493d769bb
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-25 00:20:58 +0300

    Add simple view to index.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order("updated_at ASC")
+    @post = Post.new
   end
 
   # GET /posts/1
