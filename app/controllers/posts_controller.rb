e92bb444bbb2566bade32e04a73ae227525ba97a
Ramin Mammadzada <remki-design@Hotmail.com>
2020-09-25 00:03:26 +0300

    Update Posts Controller to populate member_id when #create action is run. Redirect to the index view if post creation is ok.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,11 +25,11 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = current_user.posts.build(post_params)
+    @post = current_member.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
